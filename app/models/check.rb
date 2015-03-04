062334fc1145c3d015e680da49ebb77928949dcb
Yamada.Sn.Shizuka <Yamada.Sn.Shizuka@gmail.com>
2015-03-04 01:00:42 +0000

    kiroku の show で紐づいてる check と measurement と memo も表示するように修正中。

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,4 +1,7 @@
 class Check < ActiveRecord::Base
   belongs_to :weather
   belongs_to :kiroku 
+  belongs_to :exterior , :class_name => "Checkresult" , :foreign_key => "exterior"
+  belongs_to :tone     , :class_name => "Checkresult" , :foreign_key => "tone"
+  belongs_to :stain    , :class_name => "Checkresult" , :foreign_key => "stain"
 end
