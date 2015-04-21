f68707c3ef31cdd931460b2605367da43ec0e8e5
silica_SiO2 <yamada.sn.shizuka@gmail.com>
2015-04-21 07:02:00 +0000

    currentDate, currentMonth wo hyo-ji

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,6 @@
 class MenuController < ApplicationController
   def show
     # TODO:なんか力技なので、そのうち直す(effective_date の方をdatetimeにするのがいいのかな……)
-    @infomsgs = Infomsg.where('effective_date <= ?', Date.today.in_time_zone.to_s[0..9]).order('effective_date DESC').limit(3)
+    @infomsgs = Infomsg.where('effective_date <= ?', currentDate).order('effective_date DESC').limit(3)
   end
 end
--- app/models/concerns/common.rb
+++ app/models/concerns/common.rb
@@ -6,4 +6,13 @@ module Common
     }
     STDOUT.puts(Rails.application.class.parent_name+":"+@@h.to_json)
   end
+
+  def currentDate
+    Date.today.in_time_zone.to_s[0..9]
+  end
+
+  def currentMonth
+    Date.today.in_time_zone.to_s[0..6]
+  end
+
 end
